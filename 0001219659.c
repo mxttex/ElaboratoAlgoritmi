1e32c6f817bfba9d371082a2c5ab23c9598be2b1
mxttex <matteofaccetta@gmail.com>
2026-05-13 11:21:06 +0200

    aggiunto scheletro progetto

---
+++ 0001219659.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#define EXIT_SUCCESS 0
+/*
+
+    Matteo Faccetta,
+    0001219659,
+    Classe B,
+    matteo.faccetta@studio.unibo.it
+*/
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return EXIT_SUCCESS;
+}
